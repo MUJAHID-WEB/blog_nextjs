316da060059eb9593856e8e639bc6f143f31570a
mujahid <mujahid30390@gmail.com>
2023-12-13 17:44:09 +0600

    code

---
+++ src/app/layout.js
@@ -0,0 +1,36 @@
+
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Blog Web',
+  description: 'My best blog',
+}
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        
+      {/* <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider> */}
+              <div className="container">
+                <div className="wrapper">
+                  {/* <Navbar /> */}
+                  {children}
+                  {/* <Footer /> */}
+                </div>
+              </div>
+            {/* </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider> */}
+      
+      </body>
+    </html>
+  )
+}
